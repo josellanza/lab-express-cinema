06db77b6b5930d9d127a19bd3a57b5e2b46e30d7
Julien Chil Hagopian <julienchilhagopian@gmail.com>
2018-07-11 15:25:21 +0200

    log in - log out ok

--- routes/auth.js
+++ routes/auth.js
@@ -10,28 +10,86 @@ const saltRounds = 10;
 
 router.get('/signup', (req, res, next) => {
   // if the user is already loggedin, we redirect
-//   if (req.session.currentUser) {
-//     res.redirect('/');
-//     return;
-//   }
+  if (req.session.currentUser) {
+    res.redirect('/movie');
+    return;
+  }
   //   if the user is not login, we render the signup
   res.render('auth/signup');
 });
 
 router.post('/signup', (req, res, next) => {
-  const salt = bcrypt.genSaltSync(saltRounds);
-  const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+  if (req.session.currentUser) {
+    res.redirect('/');
+    return;
+  }
+  if (!req.body.username || !req.body.password) {
+    // message please provide a username and password
+    res.redirect('/auth/signup');
+    return;
+  }
+  User.findOne({username: req.body.username})
+    .then((user) => {
+      if (user) {
+        res.redirect('/auth/signup');
+        return;
+      }
 
-  const newUser = new User({
-    username: req.body.username,
-    password: hashedPassword
-  });
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashedPassword = bcrypt.hashSync(req.body.password, salt);
 
-  newUser.save()
-    .then(() => {
-      req.session.currentUser = newUser;
+      const newUser = new User({
+        username: req.body.username,
+        password: hashedPassword
+      });
+
+      return newUser.save()
+        .then(() => {
+          req.session.currentUser = newUser;
+          res.redirect('/movies');
+        });
+    })
+    .catch(next);
+});
+
+router.get('/login', (req, res, next) => {
+  // if the user is already loggedin, we redirect
+  if (req.session.currentUser) {
+    res.redirect('/movies');
+    return;
+  }
+  //   if the user is not login, we render the login
+  res.render('auth/login');
+});
+
+router.post('/login', (req, res, next) => {
+  if (req.session.currentUser) {
+    res.redirect('/');
+    return;
+  }
+  if (!req.body.username || !req.body.password) {
+    res.redirect('/auth/login');
+    return;
+  }
+  User.findOne({ username: req.body.username })
+    .then((user) => {
+      if (!user) {
+        res.redirect('/auth/login');
+        return;
+      }
+      if (!bcrypt.compareSync(req.body.password, user.password)) {
+        res.redirect('/auth/login');
+        return;
+      }
+      req.session.currentUser = user;
       res.redirect('/movies');
     })
     .catch(next);
 });
+
+router.post('/logout', (req, res, next) => {
+  delete req.session.currentUser;
+  res.redirect('/auth/login');
+});
+
 module.exports = router;
